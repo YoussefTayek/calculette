d872ebe368e5f6b1cb18c38f3e3c986039bc2a95
Youssef TAYEK <yousseftayek@gmail.com>
2020-12-08 11:09:41 +0100

    régler le fonctionnement des opérations

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-
+var operande1 = '';
+var operande2 = '';
+var operator = '';
 function calculer(g, v) {
    
    const ecran = document.querySelector('#ecran');
-  
+    if(v == '=') {
+
+      //Recuperer l'index de l'operateur utilisé (x,+,-,/)
+      var index = ecran.value.indexOf(operator);
+
+      //extraire la chaine à partir de l'index+1 = operande2
+      operande2 = ecran.value.substr(index+1)
+    }
    switch (g) {
     case "ch":
         if(ecran.value == '0') ecran.value = '';
         ecran.value += v;
+        
       break;
     case "dec":
          
@@ -18,39 +28,34 @@ function calculer(g, v) {
     case "op":
          operande1 = ecran.value;
          operator = v; 
-        // ecran.value = '';
-        ecran.value += v
+         ecran.value += v
       break;
     case "eg":
         switch (operator) {
            
             case "-" :
-                ecran.value = operande1 -ecran.value;
+                ecran.value = operande1 - operande2;
             break;
             
             case "+" :
-                 ecran.value = parseFloat(operande1)+parseFloat(ecran.value) ;
+                 ecran.value = parseFloat(operande1)+parseFloat(operande2) ;
             break;
             
             case "x" :
-                ecran.value = operande1 * ecran.value;
+                ecran.value = operande1 * operande2;
             break;
 
             case "/" :
-                ecran.value = operande1 / ecran.value;
+                ecran.value = operande1 / operande2;
             break;
-
         }
-       
       break;
     case "sp":
       effacer();
-      break;
-    
+      break; 
   }
+}
 
-  function effacer() {
-    ecran.value = 0;
-  }
-
+function effacer() {
+  ecran.value = 0;
 }
